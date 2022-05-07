33c8e59bcc4c3295d8bdf814f65a227a17db54f3
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-05-07 22:11:02 +0200

    Dodaj pliki projektów.

---
+++ TimePeriod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimePeriod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time = new Time(23,59,59);
+        }
+    }
+}
---
+++ TimePeriod/Time.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimePeriod
+{
+    public struct Time
+    {
+        public static void CheckTime(byte? hours = null, byte? minutes = null, byte? seconds = null)
+        {
+            if (hours != null)
+                if (hours > 23 || hours < 0)
+                    throw new ArgumentException("Niepoprawna liczba godzin");
+
+            if (minutes != null)
+                if (minutes > 59 || minutes < 0)
+                    throw new ArgumentException("Niepoprawna liczba minut");
+
+            if (seconds != null)
+                if (seconds > 59 || seconds < 0)
+                    throw new ArgumentException("Niepoprawna liczba sekund");
+
+        }
+        private readonly byte _hours;
+        private readonly byte _minutes;
+        private readonly byte _seconds;
+
+        public byte Hours => _hours;
+        public byte Minutes => _minutes;
+        public byte Seconds => _seconds;
+        
+        public Time(byte hours, byte minutes, byte seconds) {
+
+            CheckTime(hours, minutes, seconds);
+
+            _hours = hours;
+            _minutes = minutes;
+            _seconds = seconds;
+        }
+
+        public Time(byte hours, byte minutes){
+
+            CheckTime(hours, minutes);
+            _hours = hours;
+            _minutes = minutes;
+            _seconds = 0;
+        }
+
+        public Time(byte hours){
+            CheckTime(hours);
+            _hours = hours;
+            _minutes = 0;
+            _seconds = 0;
+        }
+    }
+}
